0992365d25b0cca395f7850a3dd55f6ad0bdd347
maodou <maodou24@outlook.com>
2025-05-26 23:23:31 +0800

    SQL50

---
+++ sql/sql50-postgresql/11141_pg.sql
@@ -0,0 +1,8 @@
+SELECT 
+    activity_date as day, 
+    COUNT(DISTINCT user_id) as active_users 
+FROM Activity 
+WHERE '2019-07-27' - activity_date >= 0 AND '2019-07-27' - activity_date < 30
+GROUP BY day
+
+-- date opertions  http://www.postgres.cn/docs/9.4/functions-datetime.html
