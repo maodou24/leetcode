549b3bbd22d2023bbd71abff7c7c951f82a21b8f
maodou <maodou24@outlook.com>
2025-05-28 20:39:08 +0800

    golang daily

---
+++ golang/problems/p2894_divisible_and_non_divisible_sums_difference copy.go
@@ -0,0 +1,14 @@
+package problems
+
+func differenceOfSums(n int, m int) int {
+	var num1, num2 int
+	for i := 1; i <= n; i++ {
+		if i%m == 0 {
+			num2 += i
+		} else {
+			num1 += i
+		}
+	}
+
+	return num1 - num2
+}
---
+++ golang/problems/p2894_divisible_and_non_divisible_sums_difference_test.go
@@ -0,0 +1,36 @@
+package problems
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDifferenceOfSums(t *testing.T) {
+	type data struct {
+		n      int
+		m      int
+		except int
+	}
+
+	testData := []data{
+		{
+			n:      10,
+			m:      3,
+			except: 19,
+		},
+		{
+			n:      5,
+			m:      6,
+			except: 15,
+		},
+		{
+			n:      5,
+			m:      1,
+			except: -15,
+		},
+	}
+
+	for _, tdata := range testData {
+		assert.Equal(t, tdata.except, differenceOfSums(tdata.n, tdata.m))
+	}
+}
