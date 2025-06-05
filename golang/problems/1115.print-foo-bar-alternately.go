1d5f35d8a9d24ee175efc75aa54559c9b54bf3f2
maodou <maodou24@outlook.com>
2025-06-05 21:48:56 +0800

    concurrency 1115 in go

---
+++ golang/problems/1115.print-foo-bar-alternately.go
@@ -0,0 +1,43 @@
+package problems
+/*
+ * @lc app=leetcode id=1115 lang=golang
+ *
+ * [1115] Print FooBar Alternately
+ */
+
+// @lc code=start
+type FooBar struct {
+	n int
+
+    barChan chan int
+    fooChan chan int
+}
+
+func NewFooBar(n int) *FooBar {
+	f := &FooBar{n: n}
+    f.barChan = make(chan int, 1)
+    f.fooChan = make(chan int, 1)
+
+    f.fooChan <- 1
+    return f
+}
+
+func (fb *FooBar) Foo(printFoo func()) {
+	for i := 0; i < fb.n; i++ {
+        <- fb.fooChan
+		// printFoo() outputs "foo". Do not change or remove this line.
+        printFoo()
+        fb.barChan <- 1
+	}
+}
+
+func (fb *FooBar) Bar(printBar func()) {
+	for i := 0; i < fb.n; i++ {
+        <- fb.barChan
+		// printBar() outputs "bar". Do not change or remove this line.
+        printBar()
+        fb.fooChan <- 1
+	}
+}
+// @lc code=end
+
